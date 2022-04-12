224f401b02b7978226ad95cc40ac3d030418e7eb
BrownCarter <lilbrown3000@gmail.com>
2022-04-12 10:21:58 +0100

    added main'

--- random_name.py
+++ random_name.py
@@ -1,25 +1,33 @@
+#!/bin/env python
+
 import random
 import sys
 
-first_last = (
-    "Liam", "Noah", "Oliver", "Elijah",
-    "William", "James", "Benjamin", "Lucas", "Henry", "Alexander",
-    "Mason", "Michael", "Ethan", "Daniel", "Jacob", "Logan", "Jackson",
-    "Levi", "Sebastian", "Mateo", "Jack", "Owen"
-)
 
-last_last = (
-    "Liam", "Noah", "Oliver",
-    "Elijah", "William", "James",
-    "Benjamin", "Lucas", "Henry", "Alexander",
-    "Mason", "Michael", "Ethan", "Daniel", "Jacob", "Logan", "Jackson", "Levi",
-    "Sebastian", "Mateo", "Jack", "Owen"
-)
+def main():
+    first_last = (
+        "Liam", "Noah", "Oliver", "Elijah",
+        "William", "James", "Benjamin", "Lucas", "Henry", "Alexander",
+        "Mason", "Michael", "Ethan", "Daniel", "Jacob", "Logan", "Jackson",
+        "Levi", "Sebastian", "Mateo", "Jack", "Owen"
+    )
 
-# Generate N random names
+    last_last = (
+        "Liam", "Noah", "Oliver",
+        "Elijah", "William", "James",
+        "Benjamin", "Lucas", "Henry", "Alexander",
+        "Mason", "Michael", "Ethan", "Daniel", "Jacob", "Logan", "Jackson", "Levi",
+        "Sebastian", "Mateo", "Jack", "Owen"
+    )
 
-for i in range(15):
-    fname = random.choice(first_last)
-    lname = random.choice(last_last)
+    # Generate N random names
 
-    print(f"{fname} {lname}", file=sys.stderr)
+    for i in range(15):
+        fname = random.choice(first_last)
+        lname = random.choice(last_last)
+
+        print(f"{fname} {lname}", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    main()
