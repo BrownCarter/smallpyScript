af1da485871e820506a238fd80e9718bef397827
BrownCarter <lilbrown3000@gmail.com>
2023-02-10 00:39:31 +0100

    new scripts

---
+++ switch.py
@@ -0,0 +1,22 @@
+#!/bin/env python3
+
+# user -> Brown Carter
+# file -> switch.py
+# date -> Thu-Jan-2023  07:47:42
+
+import subprocess
+from pathlib import Path
+import secrets
+
+HOME = Path().home()
+
+
+def main():
+    wall_path = Path(HOME.joinpath(".local/share/wallpapers/wall/"))
+    wallpapers = secrets.choice([wall for wall in wall_path.iterdir()])
+    subprocess.run(["swaymsg", "output", "*", "bg",
+                   wallpapers, "fill"], capture_output=True)
+
+
+if __name__ == '__main__':
+    main()
